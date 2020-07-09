57bb56b153d39e4ddcfe7014c50a463f472931f4
Andrés Javier López <ajlopez@applaudostudios.com>
2020-07-08 18:46:03 -0600

    Add package for business logic

---
+++ movies-package/movies/main.py
@@ -0,0 +1,5 @@
+"""Example file for the module"""
+
+
+def greeting():
+    return 'Hello World!'
---
+++ movies-package/setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="movies",
+    version="0.0.0",
+    author="Andres Lopez",
+    author_email="ajlopez@applaudostudios.com",
+    description="Business logic for Movie Rental application",
+    packages=setuptools.find_packages(),
+)
